fef53647baea6d94aec047701613973bf58fc98a
matrueba <matruebadev@gmail.com>
2020-10-30 00:19:55 +0100

    Add calendar instance

--- endpoints/calendar.py
+++ endpoints/calendar.py
@@ -1,1 +1,5 @@
 from flask import Blueprint, request, jsonify, make_response
+from models import Calendar
+from operations.client_operations import ClientOperations
+
+calendar = Blueprint('calendar', __name__)
